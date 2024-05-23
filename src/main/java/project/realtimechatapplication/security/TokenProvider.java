ac5a501d70b5baf110cde1d6f3782a0b45b53c9d
kyoo0115 <internetkyoomin@gmail.com>
2024-05-23 22:46:39 +0800

    feat: added token provider

---
+++ src/main/java/project/realtimechatapplication/security/TokenProvider.java
@@ -0,0 +1,48 @@
+package project.realtimechatapplication.security;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TokenProvider {
+
+    @Value("${spring.jwt.secret}")
+    private String secretKey;
+
+    public String create (String userId) {
+        Date expiredDate = Date.from(Instant.now().plus(1, ChronoUnit.HOURS));
+        Key key = Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
+
+        return Jwts.builder()
+            .signWith(key, SignatureAlgorithm.HS256)
+            .setSubject(userId)
+            .setIssuedAt(new Date()).setExpiration(expiredDate)
+            .compact();
+    }
+
+    public String validate (String jwt) {
+
+        String subject;
+        Key key = Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
+        try {
+            subject = Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(jwt)
+                .getBody()
+                .getSubject();
+
+        } catch (Exception e) {
+            return null;
+        }
+        return subject;
+    }
+}
