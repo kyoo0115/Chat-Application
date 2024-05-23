fd19c972431a8365847e8047d813d4d628f45daa
kyoo0115 <internetkyoomin@gmail.com>
2024-05-23 17:21:50 +0800

    feat: added controller for duplicateusername check

--- src/main/java/project/realtimechatapplication/controller/AuthController.java
+++ src/main/java/project/realtimechatapplication/controller/AuthController.java
@@ -1,2 +1,30 @@
-package project.realtimechatapplication.controller;public class AuthController {
+package project.realtimechatapplication.controller;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import project.realtimechatapplication.dto.request.auth.SignUpRequestDto;
+import project.realtimechatapplication.dto.request.auth.UsernameCheckRequestDto;
+import project.realtimechatapplication.dto.response.auth.SignUpResponseDto;
+import project.realtimechatapplication.dto.response.auth.UsernameCheckResponseDto;
+import project.realtimechatapplication.service.AuthService;
+
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+  private final AuthService authService;
+
+  @PostMapping("/username-check")
+  public ResponseEntity<? super UsernameCheckResponseDto> usernameCheck(
+      @RequestBody @Valid UsernameCheckRequestDto requestBody
+  ) {
+    return authService.usernameCheck(requestBody);
+  }
 }
