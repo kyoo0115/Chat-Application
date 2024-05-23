70a7371fb6a34e1de53ccb3ae3db66da6f5a1b7b
kyoo0115 <internetkyoomin@gmail.com>
2024-05-23 17:22:47 +0800

    fix: edited format for birthdate

--- src/main/java/project/realtimechatapplication/entity/UserEntity.java
+++ src/main/java/project/realtimechatapplication/entity/UserEntity.java
@@ -4,6 +4,7 @@ import static jakarta.persistence.CascadeType.ALL;
 import static jakarta.persistence.FetchType.LAZY;
 import static jakarta.persistence.GenerationType.IDENTITY;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -26,14 +27,19 @@ public class UserEntity extends BaseEntity{
   private Long id;
 
   @Column(unique = true, nullable = false)
-  private String email;
+  private String username; //로그인 아이디
+
+  @Column(unique = true, nullable = false)
+  private String email; //이메일 인증을 위해 동작
 
   @Column(nullable = false)
-  private String name;
+  private String name; //채팅에 보여줄 닉네임
 
   @Column(nullable = false)
   private String password;
 
+  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+  @Column(nullable = false)
   private LocalDate birthDate;
 
   @OneToMany(mappedBy = "user", cascade = ALL, fetch = LAZY)
