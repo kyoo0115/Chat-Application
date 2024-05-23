428ba18f3fd59ff22bf5e64e0b43637329d670a9
kyoo0115 <internetkyoomin@gmail.com>
2024-05-23 17:23:32 +0800

    feat: implemented service layer for user

--- src/main/java/project/realtimechatapplication/service/AuthService.java
+++ src/main/java/project/realtimechatapplication/service/AuthService.java
@@ -1,8 +1,13 @@
 package project.realtimechatapplication.service;
 
-import org.springframework.stereotype.Service;
+import org.springframework.http.ResponseEntity;
+import project.realtimechatapplication.dto.request.auth.UsernameCheckRequestDto;
+import project.realtimechatapplication.dto.response.auth.UsernameCheckResponseDto;
 
-@Service
-public class AuthService {
+public interface AuthService {
 
+  ResponseEntity<? super UsernameCheckResponseDto> usernameCheck(UsernameCheckRequestDto dto);
+//  ResponseEntity<? super EmailCertificationResponseDto> emailCertification(EmailCertificationRequestDto dto);
+//  ResponseEntity<? super CheckCertificationResponseDto> checkCertification(CheckCertificationRequestDto dto);
+//  ResponseEntity<? super SignUpResponseDto> signUp(SignUpRequestDto dto);
 }
--- src/main/java/project/realtimechatapplication/service/AuthServiceImpl.java
+++ src/main/java/project/realtimechatapplication/service/AuthServiceImpl.java
@@ -1,2 +1,30 @@
-package project.realtimechatapplication.service;public class AuthServiceImpl {
+package project.realtimechatapplication.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import project.realtimechatapplication.dto.request.auth.UsernameCheckRequestDto;
+import project.realtimechatapplication.dto.response.auth.UsernameCheckResponseDto;
+import project.realtimechatapplication.repository.UserRepository;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class AuthServiceImpl implements AuthService {
+
+  private final UserRepository userRepository;
+
+  @Override
+  public ResponseEntity<? super UsernameCheckResponseDto> usernameCheck(UsernameCheckRequestDto dto) {
+    try {
+      String username = dto.getUsername();
+      boolean isExist = userRepository.existsByUsername(username);
+      if(isExist) return UsernameCheckResponseDto.duplicateUsername();
+
+    } catch (Exception e) {
+      return UsernameCheckResponseDto.databaseError();
+    }
+    return UsernameCheckResponseDto.success();
+  }
 }
