1d711b5f0aefa2b7a3ced255ff43fd52ead1f98d
kyoo0115 <internetkyoomin@gmail.com>
2024-05-24 12:15:36 +0800

    feat: added constant values for authorization header and header prefix

--- src/main/java/project/realtimechatapplication/security/JwtAuthenticationFilter.java
+++ src/main/java/project/realtimechatapplication/security/JwtAuthenticationFilter.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.GrantedAuthority;
@@ -23,8 +24,11 @@ import project.realtimechatapplication.repository.UserRepository;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
+    private static final String TOKEN_PREFIX = "Bearer ";
+    private static final String AUTHORIZATION_HEADER = "Authorization";
     private final TokenProvider jwtProvider;
     private final UserRepository userRepository;
 
@@ -34,24 +38,28 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         try{
 
             String token = parseBearerToken(request);
+
             if(token == null) {
                 filterChain.doFilter(request, response);
                 return;
             }
 
-            String userId = jwtProvider.validate(token);
-            if(userId == null) {
+            String username = jwtProvider.validate(token);
+
+            if(username == null) {
                 filterChain.doFilter(request, response);
                 return;
             }
-            UserEntity user = userRepository.findByUsername(userId);
+
+            UserEntity user = userRepository.findByUsername(username);
             String role = user.getRole();
 
             List<GrantedAuthority> authorities = new ArrayList<>();
             authorities.add(new SimpleGrantedAuthority(role));
 
             SecurityContext securityContext = SecurityContextHolder.createEmptyContext();
-            AbstractAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(userId, null, authorities);
+            AbstractAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+                username, null, authorities);
 
             authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
 
@@ -59,19 +67,19 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             SecurityContextHolder.setContext(securityContext);
 
         } catch (Exception e) {
-            e.printStackTrace();
+            log.error("Error on authorization: {}", e.getMessage());
         }
         filterChain.doFilter(request, response);
     }
 
     private String parseBearerToken(HttpServletRequest request) {
 
-        String authorization = request.getHeader("Authorization");
+        String authorization = request.getHeader(AUTHORIZATION_HEADER);
 
         boolean hasAuthorization = StringUtils.hasText(authorization);
         if(!hasAuthorization) return null;
 
-        boolean isBearer = authorization.startsWith("Bearer ");
+        boolean isBearer = authorization.startsWith(TOKEN_PREFIX);
         if(!isBearer) return null;
 
         return authorization.substring(7);
