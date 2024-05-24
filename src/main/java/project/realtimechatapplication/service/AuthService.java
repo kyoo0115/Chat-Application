67f69911f81bf69f72760951157b28bcf74c8f2d
kyoo0115 <internetkyoomin@gmail.com>
2024-05-24 12:17:10 +0800

    refactor: removed unnecessary comments

--- src/main/java/project/realtimechatapplication/service/AuthService.java
+++ src/main/java/project/realtimechatapplication/service/AuthService.java
@@ -7,7 +7,4 @@ import project.realtimechatapplication.dto.response.auth.UsernameCheckResponseDt
 public interface AuthService {
 
   ResponseEntity<? super UsernameCheckResponseDto> usernameCheck(UsernameCheckRequestDto dto);
-//  ResponseEntity<? super EmailCertificationResponseDto> emailCertification(EmailCertificationRequestDto dto);
-//  ResponseEntity<? super CheckCertificationResponseDto> checkCertification(CheckCertificationRequestDto dto);
-//  ResponseEntity<? super SignUpResponseDto> signUp(SignUpRequestDto dto);
 }
