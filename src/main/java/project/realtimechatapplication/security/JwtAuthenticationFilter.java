3471d719334846d6f86445d273d75c6e15eb6927
kyoo0115 <internetkyoomin@gmail.com>
2024-05-24 18:00:41 +0800

    style: adjusted spacing by 1 line

--- src/main/java/project/realtimechatapplication/security/JwtAuthenticationFilter.java
+++ src/main/java/project/realtimechatapplication/security/JwtAuthenticationFilter.java
@@ -35,8 +35,8 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
         FilterChain filterChain) throws ServletException, IOException {
-        try{
 
+        try {
             String token = parseBearerToken(request);
 
             if(token == null) {
