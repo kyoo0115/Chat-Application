0a602e246fb5e11a8a10f5b5b2a218eb50837e4b
kyoo0115 <internetkyoomin@gmail.com>
2024-05-25 18:04:21 +0800

    feat: added request and response dto for email verification

--- src/main/java/project/realtimechatapplication/dto/request/auth/SendVerificationEmailRequestDto.java
+++ src/main/java/project/realtimechatapplication/dto/request/auth/SendVerificationEmailRequestDto.java
@@ -1,2 +1,20 @@
-package project.realtimechatapplication.dto.request.auth;public class SendVerificationEmailRequestDto {
+package project.realtimechatapplication.dto.request.auth;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class SendVerificationEmailRequestDto {
+
+  @NotBlank(message = "Invalid username: empty name")
+  private String username;
+
+  @NotBlank(message = "Invalid email: empty name")
+  @Email(message = "Invalid email format")
+  private String email;
 }
--- src/main/java/project/realtimechatapplication/dto/response/auth/SendVerificationEmailResponseDto.java
+++ src/main/java/project/realtimechatapplication/dto/response/auth/SendVerificationEmailResponseDto.java
@@ -1,2 +1,18 @@
-package project.realtimechatapplication.dto.response.auth;public class SendVerificationEmailResponseDto {
+package project.realtimechatapplication.dto.response.auth;
+
+import static org.springframework.http.HttpStatus.OK;
+
+import org.springframework.http.ResponseEntity;
+import project.realtimechatapplication.dto.response.ResponseDto;
+
+public class SendVerificationEmailResponseDto extends ResponseDto {
+
+  private SendVerificationEmailResponseDto() {
+    super();
+  }
+
+  public static ResponseEntity<? super SendVerificationEmailResponseDto> success() {
+    SendVerificationEmailResponseDto responseBody = new SendVerificationEmailResponseDto();
+    return ResponseEntity.status(OK).body(responseBody);
+  }
 }
