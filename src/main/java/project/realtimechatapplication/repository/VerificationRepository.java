9372afd100fa2e69d6f8d58f71ea12202c2dd2b2
kyoo0115 <internetkyoomin@gmail.com>
2024-05-25 18:06:56 +0800

    feat: added repository for verification

--- src/main/java/project/realtimechatapplication/repository/VerificationRepository.java
+++ src/main/java/project/realtimechatapplication/repository/VerificationRepository.java
@@ -1,2 +1,10 @@
-package project.realtimechatapplication.repository;public interface VerificationRepository {
+package project.realtimechatapplication.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import project.realtimechatapplication.entity.VerificationEntity;
+
+@Repository
+public interface VerificationRepository extends JpaRepository<VerificationEntity, Long>  {
+
 }
