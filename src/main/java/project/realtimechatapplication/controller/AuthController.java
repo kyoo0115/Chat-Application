9a64bc75ceb818760e86fb45cf207ca730e94d71
kyoo0115 <internetkyoomin@gmail.com>
2024-05-25 19:43:34 +0800

    feat: added post mapping for checking verification

--- src/main/java/project/realtimechatapplication/controller/AuthController.java
+++ src/main/java/project/realtimechatapplication/controller/AuthController.java
@@ -7,8 +7,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import project.realtimechatapplication.dto.request.auth.CheckVerificationRequestDto;
 import project.realtimechatapplication.dto.request.auth.SendVerificationEmailRequestDto;
 import project.realtimechatapplication.dto.request.auth.UsernameCheckRequestDto;
+import project.realtimechatapplication.dto.response.auth.CheckVerificationResponseDto;
 import project.realtimechatapplication.dto.response.auth.SendVerificationEmailResponseDto;
 import project.realtimechatapplication.dto.response.auth.UsernameCheckResponseDto;
 import project.realtimechatapplication.service.AuthService;
@@ -35,5 +37,13 @@ public class AuthController {
   ) {
     authService.sendVerificationEmail(dto);
     return SendVerificationEmailResponseDto.success();
+  }
+
+  @PostMapping("/check-verification-email")
+  public ResponseEntity<? super CheckVerificationResponseDto> checkVerification(
+      @RequestBody @Valid CheckVerificationRequestDto dto
+  ) {
+    authService.checkVerification(dto);
+    return CheckVerificationResponseDto.success();
   }
 }
