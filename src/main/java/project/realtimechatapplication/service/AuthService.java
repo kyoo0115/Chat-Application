236c4e595d34b342d1eec4db78e1d618cdf016da
kyoo0115 <internetkyoomin@gmail.com>
2024-05-25 18:03:18 +0800

    feat: added service for sending verification email

--- src/main/java/project/realtimechatapplication/service/AuthService.java
+++ src/main/java/project/realtimechatapplication/service/AuthService.java
@@ -1,10 +1,10 @@
 package project.realtimechatapplication.service;
 
-import org.springframework.http.ResponseEntity;
+import project.realtimechatapplication.dto.request.auth.SendVerificationEmailRequestDto;
 import project.realtimechatapplication.dto.request.auth.UsernameCheckRequestDto;
-import project.realtimechatapplication.dto.response.auth.UsernameCheckResponseDto;
 
 public interface AuthService {
 
   void usernameCheck(UsernameCheckRequestDto dto);
+  void sendVerificationEmail(SendVerificationEmailRequestDto dto);
 }
--- src/main/java/project/realtimechatapplication/service/AuthServiceImpl.java
+++ src/main/java/project/realtimechatapplication/service/AuthServiceImpl.java
@@ -2,11 +2,14 @@ package project.realtimechatapplication.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import project.realtimechatapplication.dto.request.auth.SendVerificationEmailRequestDto;
 import project.realtimechatapplication.dto.request.auth.UsernameCheckRequestDto;
-import project.realtimechatapplication.dto.response.auth.UsernameCheckResponseDto;
+import project.realtimechatapplication.entity.VerificationEntity;
+import project.realtimechatapplication.model.VerificationNumberGenerator;
+import project.realtimechatapplication.provider.EmailProvider;
 import project.realtimechatapplication.repository.UserRepository;
+import project.realtimechatapplication.repository.VerificationRepository;
 
 @Service
 @RequiredArgsConstructor
@@ -14,13 +17,40 @@ import project.realtimechatapplication.repository.UserRepository;
 public class AuthServiceImpl implements AuthService {
 
   private final UserRepository userRepository;
+  private final VerificationRepository verificationRepository;
+
+  private final EmailProvider emailProvider;
 
   @Override
   public void usernameCheck(UsernameCheckRequestDto dto) {
 
     userRepository.findByUsername(dto.getUsername())
         .ifPresent(user -> {
-          throw new RuntimeException("이미 존재하는 사용자 이름입니다.");
+          throw new RuntimeException("User already exists.");
         });
+  }
+
+  @Override
+  public void sendVerificationEmail(SendVerificationEmailRequestDto dto) {
+
+    userRepository.findByUsername(dto.getUsername())
+        .ifPresent(user -> {
+          throw new RuntimeException("User already exists.");
+        });
+
+    userRepository.findByEmail(dto.getEmail())
+        .ifPresent(user -> {
+          throw new RuntimeException("Email already exists.");
+        });
+
+    String verificationNumber = VerificationNumberGenerator.generateVerificationNumber();
+    boolean sendMail = emailProvider.sendVerificationEmail(dto.getEmail(), (verificationNumber));
+
+    if (!sendMail) {
+      throw new RuntimeException("Email send failure.");
+    }
+
+    VerificationEntity verificationEntity = VerificationEntity.of(dto.getUsername(), dto.getEmail(), verificationNumber);
+    verificationRepository.save(verificationEntity);
   }
 }
