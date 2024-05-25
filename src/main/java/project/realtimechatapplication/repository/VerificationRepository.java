51a0ea22c4a0f7bd0c656ba089bcb64c887c438b
kyoo0115 <internetkyoomin@gmail.com>
2024-05-25 19:45:17 +0800

    feat: added repository for finding username

--- src/main/java/project/realtimechatapplication/repository/VerificationRepository.java
+++ src/main/java/project/realtimechatapplication/repository/VerificationRepository.java
@@ -1,5 +1,7 @@
 package project.realtimechatapplication.repository;
 
+import java.util.Optional;
+import javax.swing.text.html.Option;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import project.realtimechatapplication.entity.VerificationEntity;
@@ -7,4 +9,7 @@ import project.realtimechatapplication.entity.VerificationEntity;
 @Repository
 public interface VerificationRepository extends JpaRepository<VerificationEntity, Long>  {
 
+  Optional<VerificationEntity> findByUsernameAndEmail(String username, String email);
+
+  Optional<VerificationEntity> findByUsername(String username);
 }
