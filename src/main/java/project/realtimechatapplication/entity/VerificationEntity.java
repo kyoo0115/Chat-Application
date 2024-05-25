4a5324d70630c932f28439548a535d561c403c1a
kyoo0115 <internetkyoomin@gmail.com>
2024-05-25 18:05:22 +0800

    feat: created static method for creating entity

--- src/main/java/project/realtimechatapplication/entity/VerificationEntity.java
+++ src/main/java/project/realtimechatapplication/entity/VerificationEntity.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -12,11 +13,21 @@ import lombok.NoArgsConstructor;
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class VerificationEntity extends BaseEntity {
 
   @Id
-  private long senderId;
+  private String username;
+
   private String email;
   private String verificationNumber;
+
+  public static VerificationEntity of(String username, String email, String verificationNumber) {
+    return VerificationEntity.builder()
+        .username(username)
+        .email(email)
+        .verificationNumber(verificationNumber)
+        .build();
+  }
 }
 
