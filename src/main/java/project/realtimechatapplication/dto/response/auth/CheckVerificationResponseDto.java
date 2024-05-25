4a771af24fde7400796fd94108eed9fe063eaa1e
kyoo0115 <internetkyoomin@gmail.com>
2024-05-25 19:44:38 +0800

    feat: added response dto for verification check

--- src/main/java/project/realtimechatapplication/dto/response/auth/CheckVerificationResponseDto.java
+++ src/main/java/project/realtimechatapplication/dto/response/auth/CheckVerificationResponseDto.java
@@ -1,2 +1,20 @@
-package project.realtimechatapplication.dto.response.auth;public class CheckVerificationResponseDto {
+package project.realtimechatapplication.dto.response.auth;
+
+import static org.springframework.http.HttpStatus.OK;
+
+import org.springframework.http.ResponseEntity;
+import project.realtimechatapplication.dto.response.ResponseDto;
+
+public class CheckVerificationResponseDto extends ResponseDto {
+
+  private CheckVerificationResponseDto() {
+    super();
+  }
+
+  public static ResponseEntity<? super CheckVerificationResponseDto> success() {
+    ResponseDto responseBody = new CheckVerificationResponseDto();
+    return ResponseEntity.status(OK).body(responseBody);
+  }
 }
+
+
