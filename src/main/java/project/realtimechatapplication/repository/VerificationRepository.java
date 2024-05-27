1d51685743161d4173780645d21af79f456a1d16
kyoo0115 <internetkyoomin@gmail.com>
2024-05-27 22:56:13 +0800

    refactor: deleted unnecessary method

--- src/main/java/project/realtimechatapplication/repository/VerificationRepository.java
+++ src/main/java/project/realtimechatapplication/repository/VerificationRepository.java
@@ -1,7 +1,6 @@
 package project.realtimechatapplication.repository;
 
 import java.util.Optional;
-import javax.swing.text.html.Option;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import project.realtimechatapplication.entity.VerificationEntity;
@@ -9,9 +8,6 @@ import project.realtimechatapplication.entity.VerificationEntity;
 @Repository
 public interface VerificationRepository extends JpaRepository<VerificationEntity, Long>  {
 
-  Optional<VerificationEntity> findByUsernameAndEmail(String username, String email);
-
   Optional<VerificationEntity> findByUsername(String username);
-
   void deleteByUsername(String username);
 }
