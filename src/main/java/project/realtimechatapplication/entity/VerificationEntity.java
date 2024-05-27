b9ef72b446a3dbbfedeceedeac29093095cc8c22
kyoo0115 <internetkyoomin@gmail.com>
2024-05-27 13:35:09 +0800

    feat: added static method for verification entity

--- src/main/java/project/realtimechatapplication/entity/VerificationEntity.java
+++ src/main/java/project/realtimechatapplication/entity/VerificationEntity.java
@@ -7,6 +7,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import project.realtimechatapplication.dto.request.auth.SendVerificationEmailRequestDto;
 
 @Entity(name = "verification")
 @Table(name = "verification")
@@ -22,10 +23,10 @@ public class VerificationEntity extends BaseEntity {
   private String email;
   private String verificationNumber;
 
-  public static VerificationEntity of(String username, String email, String verificationNumber) {
+  public static VerificationEntity of(SendVerificationEmailRequestDto dto, String verificationNumber) {
     return VerificationEntity.builder()
-        .username(username)
-        .email(email)
+        .username(dto.getUsername())
+        .email(dto.getEmail())
         .verificationNumber(verificationNumber)
         .build();
   }
