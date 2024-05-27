2754e30e55d14b911bd0a2498884bdb1cd72b4ef
kyoo0115 <internetkyoomin@gmail.com>
2024-05-27 13:33:49 +0800

    feat: added message for validation handling

--- src/main/java/project/realtimechatapplication/dto/request/auth/SignUpRequestDto.java
+++ src/main/java/project/realtimechatapplication/dto/request/auth/SignUpRequestDto.java
@@ -8,30 +8,32 @@ import java.time.LocalDate;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
 
 @Getter
 @Setter
 @NoArgsConstructor
 public class SignUpRequestDto {
 
-  @NotBlank
+  @NotBlank(message = "Username cannot be blank.")
   private String username;
 
-  @NotBlank
+  @NotBlank(message = "Name cannot be blank.")
   private String name;
 
-  @NotBlank
-  @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*[0-9])[a-zA-Z0-9]{8,13}$")
+  @NotBlank(message = "Password cannot be blank.")
+  @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*[0-9])[a-zA-Z0-9]{8,13}$", message = "Invalid Format Password")
   private String password;
 
-  @Email
-  @NotBlank
+  @Email(message = "Invalid email format")
+  @NotBlank(message = "Email cannot be blank.")
   private String email;
 
-  @NotBlank
+  @NotBlank(message = "Verification number cannot be blank.")
+  @Length(min = 4, max = 4, message = "Invalid verification number: length should be 4")
   private String verificationNumber;
 
-  @Past
+  @Past(message = "Invalid Birth Date, should be YYYY-MM-DD format")
   private LocalDate birthDate;
 
 }
