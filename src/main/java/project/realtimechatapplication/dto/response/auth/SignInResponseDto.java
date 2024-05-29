fc9879a19f7d6181ec22990418bb640b32742914
kyoo0115 <internetkyoomin@gmail.com>
2024-05-29 22:56:36 +0800

    feat: created dto for response sign in

--- src/main/java/project/realtimechatapplication/dto/response/auth/SignInResponseDto.java
+++ src/main/java/project/realtimechatapplication/dto/response/auth/SignInResponseDto.java
@@ -1,2 +1,29 @@
-package project.realtimechatapplication.dto.response.auth;public class SignInResponseDto {
+package project.realtimechatapplication.dto.response.auth;
+
+import static org.springframework.http.HttpStatus.OK;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import org.springframework.http.ResponseEntity;
+import project.realtimechatapplication.dto.response.ResponseDto;
+
+@Getter
+public class SignInResponseDto extends ResponseDto {
+
+  @JsonProperty("token")
+  private final String token;
+
+  @JsonProperty("expirationTime")
+  private final int expirationTime;
+
+  private SignInResponseDto(String token) {
+    super();
+    this.token = token;
+    this.expirationTime = 3600;
+  }
+
+  public static ResponseEntity<SignInResponseDto> authenticate(String token) {
+    SignInResponseDto responseBody = new SignInResponseDto(token);
+    return ResponseEntity.status(OK).body(responseBody);
+  }
 }
