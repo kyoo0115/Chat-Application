88c81e96d39b98b71176aa4826b18e90fbe2334b
kyoo0115 <internetkyoomin@gmail.com>
2024-05-29 23:00:10 +0800

    feat: added controller for oauth2

--- src/main/java/project/realtimechatapplication/controller/OAuthController.java
+++ src/main/java/project/realtimechatapplication/controller/OAuthController.java
@@ -1,2 +1,30 @@
-package project.realtimechatapplication.controller;public class OauthController {
+package project.realtimechatapplication.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import project.realtimechatapplication.service.impl.OAuth2UserServiceImpl;
+
+@RequestMapping("/auth")
+@RestController
+@RequiredArgsConstructor
+public class OAuthController {
+
+  private final OAuth2UserServiceImpl oAuth2UserService;
+
+  @PostMapping("/google")
+  public String googleSignIn() {
+    return "google";
+  }
+
+//  @PostMapping("/oauth2/kakao")
+//  public String kakaoSignIn(@RequestBody @Valid OAuthRequestDto requestDto) {
+//    return oAuth2UserService.processOAuthSignIn("kakao", requestDto);
+//  }
+
+  @PostMapping("/callback/naver")
+  public String naverSignIn() {
+    return "naver";
+  }
 }
