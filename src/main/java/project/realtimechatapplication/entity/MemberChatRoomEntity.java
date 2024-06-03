e3e6be44d9236994d5b89d5196da2b1c36c0663e
kyoo0115 <internetkyoomin@gmail.com>
2024-06-04 02:11:26 +0800

    feat: added additional entity to prevent many to many relationship for member and chatroos

--- src/main/java/project/realtimechatapplication/entity/MemberChatRoomEntity.java
+++ src/main/java/project/realtimechatapplication/entity/MemberChatRoomEntity.java
@@ -1,2 +1,42 @@
-package project.realtimechatapplication.entity;public class MemberChatRoomEntity {
+package project.realtimechatapplication.entity;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "member_chat_room")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class MemberChatRoomEntity {
+
+  @Id
+  @GeneratedValue(strategy = IDENTITY)
+  private Long id;
+
+  @ManyToOne
+  @JoinColumn(name = "chat_room_id", nullable = false)
+  private ChatRoomEntity chatRoom;
+
+  @ManyToOne
+  @JoinColumn(name = "user_id", nullable = false)
+  private UserEntity user;
+
+  public static MemberChatRoomEntity of(ChatRoomEntity chatRoom, UserEntity user) {
+    MemberChatRoomEntity memberChatRoom = new MemberChatRoomEntity();
+    memberChatRoom.setChatRoom(chatRoom);
+    memberChatRoom.setUser(user);
+    return memberChatRoom;
+  }
 }
