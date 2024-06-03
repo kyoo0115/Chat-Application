aff6e13fd0cd078ebd4e5c6106c6849772471a09
kyoo0115 <internetkyoomin@gmail.com>
2024-06-04 02:10:09 +0800

    feat: added repository for memberchatroom

--- src/main/java/project/realtimechatapplication/repository/MemberChatRoomRepository.java
+++ src/main/java/project/realtimechatapplication/repository/MemberChatRoomRepository.java
@@ -1,2 +1,16 @@
-package project.realtimechatapplication.repository;public class MemberChatRoomRepository {
+package project.realtimechatapplication.repository;
+
+import java.util.List;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import project.realtimechatapplication.entity.ChatRoomEntity;
+import project.realtimechatapplication.entity.MemberChatRoomEntity;
+import project.realtimechatapplication.entity.UserEntity;
+
+@Repository
+public interface MemberChatRoomRepository extends JpaRepository<MemberChatRoomEntity, Long> {
+  List<MemberChatRoomEntity> findByUser(UserEntity user);
+
+  Optional<MemberChatRoomEntity> findByUserAndChatRoom(UserEntity user, ChatRoomEntity chatRoom);
 }
