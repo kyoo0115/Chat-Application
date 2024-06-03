72bc954dafe3e448b7a3938893c4612b82f6f8c2
kyoo0115 <internetkyoomin@gmail.com>
2024-06-04 02:03:01 +0800

    feat: added service for message

--- src/main/java/project/realtimechatapplication/service/MessageService.java
+++ src/main/java/project/realtimechatapplication/service/MessageService.java
@@ -1,2 +1,13 @@
-package project.realtimechatapplication.service;public interface MessageService {
+package project.realtimechatapplication.service;
+
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import project.realtimechatapplication.dto.request.chat.ChatDto;
+import project.realtimechatapplication.dto.response.chat.MessageSendResponseDto;
+import project.realtimechatapplication.model.type.MessageType;
+
+public interface MessageService {
+
+    MessageSendResponseDto sendMessage(ChatDto chatDto);
+
+    ChatDto makeEnterMessageAndSetSessionAttribute(ChatDto chatDto, SimpMessageHeaderAccessor headerAccessor);
 }
