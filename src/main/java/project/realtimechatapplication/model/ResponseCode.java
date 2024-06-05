acccfe2adf55c272b39bf7e73f406d331c9b08d3
kyoo0115 <internetkyoomin@gmail.com>
2024-06-05 15:35:37 +0800

    feat: removed unused variables

--- src/main/java/project/realtimechatapplication/model/ResponseCode.java
+++ src/main/java/project/realtimechatapplication/model/ResponseCode.java
@@ -3,12 +3,4 @@ package project.realtimechatapplication.model;
 public interface ResponseCode {
 
   String SUCCESS = "SU";
-  String VALIDATION_FAIL = "VF";
-  String DUPLICATE_ID = "DI";
-
-  String SIGN_IN_FAIL = "SF";
-  String VERIFICATION_FAIL = "VF";
-
-  String MAIL_FAIL = "MF";
-  String DATABASE_ERROR = "DBE";
 }
