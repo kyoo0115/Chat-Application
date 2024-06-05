fb17ba1b35990c07c6131b4af11f143157ba830a
kyoo0115 <internetkyoomin@gmail.com>
2024-06-05 15:33:58 +0800

    feat: changed parameter to dto

--- src/main/java/project/realtimechatapplication/service/ChatRoomService.java
+++ src/main/java/project/realtimechatapplication/service/ChatRoomService.java
@@ -1,12 +1,16 @@
 package project.realtimechatapplication.service;
 
 import java.util.List;
+import org.apache.catalina.User;
+import project.realtimechatapplication.dto.request.chat.ChatDto;
+import project.realtimechatapplication.dto.request.chat.ChatRoomAddRequestDto;
+import project.realtimechatapplication.dto.request.chat.ChatRoomCreateRequestDto;
 import project.realtimechatapplication.dto.request.chat.ChatRoomDto;
 
 public interface ChatRoomService {
-    ChatRoomDto createChatRoom(String roomName, String username);
+    ChatRoomDto createChatRoom(ChatRoomCreateRequestDto dto, String username);
 
     List<ChatRoomDto> searchChatRoomList(String username);
 
-    ChatRoomDto addMemberToChatRoom(String roomCode, String username, String memberToAdd);
+    ChatRoomDto addMemberToChatRoom(ChatRoomAddRequestDto dto);
 }
