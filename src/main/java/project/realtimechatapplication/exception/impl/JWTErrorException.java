c6d486972ed53b3f6d8f2e27956841aa24126cc1
kyoo0115 <internetkyoomin@gmail.com>
2024-06-05 15:34:35 +0800

    feat: added additional exception

--- src/main/java/project/realtimechatapplication/exception/impl/JWTErrorException.java
+++ src/main/java/project/realtimechatapplication/exception/impl/JWTErrorException.java
@@ -1,2 +1,18 @@
-package project.realtimechatapplication.exception.impl;public class JWTErrorException {
+package project.realtimechatapplication.exception.impl;
+
+import org.springframework.http.HttpStatus;
+import project.realtimechatapplication.exception.AbstractException;
+
+public class JWTErrorException extends AbstractException {
+
+
+  @Override
+  public int getStatusCode() {
+    return HttpStatus.UNAUTHORIZED.value();
+  }
+
+  @Override
+  public String getMessage() {
+    return "JWT token is not valid.";
+  }
 }
