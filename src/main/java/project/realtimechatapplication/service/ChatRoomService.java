77410a4bb2d1ee77e9ec2514dbc6440a239fd199
kyoo0115 <internetkyoomin@gmail.com>
2024-06-07 17:24:41 +0800

    feat: added service methods for CRUd

--- src/main/java/project/realtimechatapplication/service/ChatRoomService.java
+++ src/main/java/project/realtimechatapplication/service/ChatRoomService.java
@@ -1,16 +1,19 @@
 package project.realtimechatapplication.service;
 
 import java.util.List;
-import org.apache.catalina.User;
-import project.realtimechatapplication.dto.request.chat.ChatDto;
 import project.realtimechatapplication.dto.request.chat.ChatRoomAddRequestDto;
 import project.realtimechatapplication.dto.request.chat.ChatRoomCreateRequestDto;
 import project.realtimechatapplication.dto.request.chat.ChatRoomDto;
 
 public interface ChatRoomService {
-    ChatRoomDto createChatRoom(ChatRoomCreateRequestDto dto, String username);
 
-    List<ChatRoomDto> searchChatRoomList(String username);
+  ChatRoomDto createChatRoom(ChatRoomCreateRequestDto dto, String username);
 
-    ChatRoomDto addMemberToChatRoom(ChatRoomAddRequestDto dto);
+  List<ChatRoomDto> searchChatRoomList(String username);
+
+  ChatRoomDto addMemberToChatRoom(ChatRoomAddRequestDto dto, String username, Long memberId);
+
+  ChatRoomDto removeMemberFromChatRoom(Long userId, Long roomId, String username);
+
+  ChatRoomDto deleteChatRoom(Long roomId, String username);
 }
