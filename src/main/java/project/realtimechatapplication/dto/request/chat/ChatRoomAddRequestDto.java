ba68f817cbbe0b2ed80f6fdbaac08a69a3154eba
kyoo0115 <internetkyoomin@gmail.com>
2024-06-07 17:19:22 +0800

    feat: added request dto for adding chatroom

--- src/main/java/project/realtimechatapplication/dto/request/chat/ChatRoomAddRequestDto.java
+++ src/main/java/project/realtimechatapplication/dto/request/chat/ChatRoomAddRequestDto.java
@@ -1,18 +1,11 @@
 package project.realtimechatapplication.dto.request.chat;
 
-import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class ChatRoomAddRequestDto {
-
-  @NotBlank(message = "Invalid username: empty name")
-  private String username;
-
-  @NotBlank(message = "Invalid room code: empty room code")
   private String roomCode;
   private boolean isPrivate;
 }
