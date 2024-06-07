9a32e5262d16c93958680395cf280c93ea52bde5
kyoo0115 <internetkyoomin@gmail.com>
2024-06-07 17:17:18 +0800

    feat: added service for CRUD for message

--- src/main/java/project/realtimechatapplication/service/MessageService.java
+++ src/main/java/project/realtimechatapplication/service/MessageService.java
@@ -2,12 +2,20 @@ package project.realtimechatapplication.service;
 
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import project.realtimechatapplication.dto.request.chat.ChatDto;
+import project.realtimechatapplication.dto.request.chat.DeleteMessageRequestDto;
+import project.realtimechatapplication.dto.request.chat.EditMessageRequestDto;
+import project.realtimechatapplication.dto.response.chat.DeleteMessageResponseDto;
+import project.realtimechatapplication.dto.response.chat.EditMessageResponseDto;
 import project.realtimechatapplication.dto.response.chat.MessageSendResponseDto;
-import project.realtimechatapplication.model.type.MessageType;
 
 public interface MessageService {
 
-    MessageSendResponseDto sendMessage(ChatDto chatDto);
+  MessageSendResponseDto sendMessage(ChatDto dto);
 
-    ChatDto makeEnterMessageAndSetSessionAttribute(ChatDto chatDto, SimpMessageHeaderAccessor headerAccessor);
+  ChatDto makeEnterMessageAndSetSessionAttribute(ChatDto dto,
+      SimpMessageHeaderAccessor headerAccessor);
+
+  EditMessageResponseDto editMessage(EditMessageRequestDto dto, String username, Long messageId);
+
+  DeleteMessageResponseDto deleteMessage(DeleteMessageRequestDto dto, String username, Long messageId);
 }
