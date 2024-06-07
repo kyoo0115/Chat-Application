7f0a7b2730f930eadca86806afd83bfff4af492c
kyoo0115 <internetkyoomin@gmail.com>
2024-06-07 17:17:44 +0800

    feat: added dto to check the membership of the room to reduce queries

--- src/main/java/project/realtimechatapplication/dto/ChatRoomMembershipDto.java
+++ src/main/java/project/realtimechatapplication/dto/ChatRoomMembershipDto.java
@@ -1,2 +1,17 @@
-package project.realtimechatapplication.dto;public class ChatRoomMembershipDto {
+package project.realtimechatapplication.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class ChatRoomMembershipDto {
+
+  private Long chatRoomId;
+  private String requestUsername;
+  private String ownerUsername;
+  private boolean isMemberAlreadyInChatRoom;
+
 }
