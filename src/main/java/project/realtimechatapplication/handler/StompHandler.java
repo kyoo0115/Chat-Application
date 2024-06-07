9bb37856d9220c970065a1990481ea0dda07309a
kyoo0115 <internetkyoomin@gmail.com>
2024-06-07 17:24:07 +0800

    style: fixed the indentation

--- src/main/java/project/realtimechatapplication/handler/StompHandler.java
+++ src/main/java/project/realtimechatapplication/handler/StompHandler.java
@@ -1,6 +1,5 @@
 package project.realtimechatapplication.handler;
 
-import java.util.Objects;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.Message;
