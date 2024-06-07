2829aea1954060fdede47796f62a788d0922bf29
kyoo0115 <internetkyoomin@gmail.com>
2024-06-07 17:32:47 +0800

    feat: added service layer for chatroom crud

--- src/main/java/project/realtimechatapplication/service/impl/ChatRoomServiceImpl.java
+++ src/main/java/project/realtimechatapplication/service/impl/ChatRoomServiceImpl.java
@@ -5,6 +5,7 @@ import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import project.realtimechatapplication.dto.ChatRoomMembershipDto;
 import project.realtimechatapplication.dto.request.chat.ChatRoomAddRequestDto;
 import project.realtimechatapplication.dto.request.chat.ChatRoomCreateRequestDto;
 import project.realtimechatapplication.dto.request.chat.ChatRoomDto;
@@ -12,6 +13,9 @@ import project.realtimechatapplication.entity.ChatRoomEntity;
 import project.realtimechatapplication.entity.MemberChatRoomEntity;
 import project.realtimechatapplication.entity.UserEntity;
 import project.realtimechatapplication.exception.impl.ChatRoomNotFoundException;
+import project.realtimechatapplication.exception.impl.MemberAlreadyInChatroomException;
+import project.realtimechatapplication.exception.impl.MemberNotInChatroomException;
+import project.realtimechatapplication.exception.impl.UnauthorizedRoomOwnerException;
 import project.realtimechatapplication.exception.impl.UserNotFoundException;
 import project.realtimechatapplication.repository.ChatRoomRepository;
 import project.realtimechatapplication.repository.MemberChatRoomRepository;
@@ -20,51 +24,104 @@ import project.realtimechatapplication.service.ChatRoomService;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class ChatRoomServiceImpl implements ChatRoomService {
 
-    private final ChatRoomRepository chatRoomRepository;
-    private final UserRepository userRepository;
-    private final MemberChatRoomRepository memberChatRoomRepository;
+  private final ChatRoomRepository chatRoomRepository;
+  private final UserRepository userRepository;
+  private final MemberChatRoomRepository memberChatRoomRepository;
 
-    @Override
-    @Transactional
-    public ChatRoomDto createChatRoom(ChatRoomCreateRequestDto dto, String username) {
+  @Override
+  @Transactional
+  public ChatRoomDto createChatRoom(ChatRoomCreateRequestDto dto, String username) {
 
-        ChatRoomEntity chatRoom = ChatRoomEntity.of(dto.getRoomName());
+    UserEntity user = userRepository.findByUsername(username)
+        .orElseThrow(UserNotFoundException::new);
 
-        UserEntity user = userRepository.findByUsername(username)
-            .orElseThrow(UserNotFoundException::new);
+    ChatRoomEntity chatRoom = ChatRoomEntity.of(dto.getRoomName(), user.getUsername());
 
-        chatRoomRepository.save(chatRoom);
-        memberChatRoomRepository.save(MemberChatRoomEntity.of(chatRoom, user));
+    chatRoomRepository.save(chatRoom);
+    memberChatRoomRepository.save(MemberChatRoomEntity.of(chatRoom, user));
 
-        return ChatRoomDto.from(chatRoom);
+    return ChatRoomDto.from(chatRoom);
+  }
+
+  @Override
+  @Transactional
+  public ChatRoomDto addMemberToChatRoom(ChatRoomAddRequestDto dto, String username, Long memberId) {
+
+    ChatRoomMembershipDto membershipDto = memberChatRoomRepository.checkChatRoomAndUserDetails(dto.getRoomCode(), username, memberId)
+        .orElseThrow(ChatRoomNotFoundException::new);
+
+    if (!membershipDto.getOwnerUsername().equals(username)) {
+      throw new UnauthorizedRoomOwnerException();
     }
 
-    @Override
-    @Transactional
-    public ChatRoomDto addMemberToChatRoom(ChatRoomAddRequestDto dto) {
-        ChatRoomEntity chatRoom = chatRoomRepository.findByRoomCode(dto.getRoomCode())
-            .orElseThrow(ChatRoomNotFoundException::new);
-
-        UserEntity member = userRepository.findByUsername(dto.getUsername())
-            .orElseThrow(UserNotFoundException::new);
-
-        if (memberChatRoomRepository.findByUserAndChatRoom(member, chatRoom).isEmpty()) {
-            memberChatRoomRepository.save(MemberChatRoomEntity.of(chatRoom, member));
-        }
-
-        return ChatRoomDto.from(chatRoom);
+    if (membershipDto.isMemberAlreadyInChatRoom()) {
+      throw new MemberAlreadyInChatroomException();
     }
 
-    @Override
-    @Transactional(readOnly = true)
-    public List<ChatRoomDto> searchChatRoomList(String username) {
+    UserEntity member = userRepository.findById(memberId)
+        .orElseThrow(UserNotFoundException::new);
 
-        List<ChatRoomEntity> chatRooms = memberChatRoomRepository.findChatRoomsByUsername(username);
+    ChatRoomEntity chatRoom = chatRoomRepository.getReferenceById(membershipDto.getChatRoomId());
+    memberChatRoomRepository.save(MemberChatRoomEntity.of(chatRoom, member));
 
-        return chatRooms.stream()
-            .map(ChatRoomDto::from)
-            .collect(Collectors.toList());
+    return ChatRoomDto.from(chatRoom);
+  }
+
+  @Override
+  @Transactional
+  public ChatRoomDto deleteChatRoom(Long roomId, String username) {
+
+    ChatRoomEntity chatRoom = chatRoomRepository.findById(roomId)
+        .orElseThrow(ChatRoomNotFoundException::new);
+
+    UserEntity user = userRepository.findByUsername(username)
+        .orElseThrow(UserNotFoundException::new);
+
+    if (!chatRoom.getOwner().equals(user.getUsername())) {
+      throw new UnauthorizedRoomOwnerException();
     }
+
+    chatRoomRepository.delete(chatRoom);
+
+    return ChatRoomDto.from(chatRoom);
+  }
+
+  @Override
+  @Transactional
+  public ChatRoomDto removeMemberFromChatRoom(Long userId, Long roomId, String username) {
+
+    ChatRoomMembershipDto membershipDto = memberChatRoomRepository.checkChatRoomAndUserDetailsById(roomId, username, userId)
+        .orElseThrow(ChatRoomNotFoundException::new);
+
+    if (!membershipDto.getOwnerUsername().equals(username)) {
+       throw new UnauthorizedRoomOwnerException();
+    }
+
+    if (!membershipDto.isMemberAlreadyInChatRoom()) {
+      throw new MemberAlreadyInChatroomException();
+    }
+
+    UserEntity member = userRepository.findById(userId)
+        .orElseThrow(UserNotFoundException::new);
+
+    MemberChatRoomEntity memberChatRoom = memberChatRoomRepository.findByUserAndChatRoom(member, chatRoomRepository.getReferenceById(roomId))
+        .orElseThrow(MemberNotInChatroomException::new);
+    memberChatRoomRepository.delete(memberChatRoom);
+
+    ChatRoomEntity chatRoom = chatRoomRepository.getReferenceById(membershipDto.getChatRoomId());
+    return ChatRoomDto.from(chatRoom);
+  }
+
+  @Override
+  public List<ChatRoomDto> searchChatRoomList(String username) {
+
+    List<ChatRoomEntity> chatRooms = memberChatRoomRepository.findChatRoomsByUsername(username);
+
+    return chatRooms.stream()
+        .map(ChatRoomDto::from)
+        .collect(Collectors.toList());
+  }
 }
