86a571633a834ab254f8e27432becb054bc164e6
kyoo0115 <internetkyoomin@gmail.com>
2024-06-07 17:19:44 +0800

    feat: added request dto for deleting message

--- src/main/java/project/realtimechatapplication/dto/request/chat/DeleteMessageRequestDto.java
+++ src/main/java/project/realtimechatapplication/dto/request/chat/DeleteMessageRequestDto.java
@@ -1,2 +1,12 @@
-package project.realtimechatapplication.dto.request.chat;public class DeleteMessageRequestDto {
+package project.realtimechatapplication.dto.request.chat;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DeleteMessageRequestDto {
+
+  private String sender;
+  private String roomCode;
 }
