1144a79792efe0decf98a28986ac907ab3de12b7
kyoo0115 <internetkyoomin@gmail.com>
2024-06-07 17:34:19 +0800

    feat: added exceptions

--- src/main/java/project/realtimechatapplication/exception/impl/MessageNotExistException.java
+++ src/main/java/project/realtimechatapplication/exception/impl/MessageNotExistException.java
@@ -1,2 +1,17 @@
-package project.realtimechatapplication.exception.impl;public class MessageNotExistException {
+package project.realtimechatapplication.exception.impl;
+
+import org.springframework.http.HttpStatus;
+import project.realtimechatapplication.exception.AbstractException;
+
+public class MessageNotExistException extends AbstractException {
+
+  @Override
+  public int getStatusCode() {
+    return HttpStatus.NOT_FOUND.value();
+  }
+
+  @Override
+  public String getMessage() {
+    return "Message not exist.";
+  }
 }
--- src/main/java/project/realtimechatapplication/exception/impl/UnauthorizedRoomOwnerException.java
+++ src/main/java/project/realtimechatapplication/exception/impl/UnauthorizedRoomOwnerException.java
@@ -1,2 +1,17 @@
-package project.realtimechatapplication.exception.impl;public class UnauthorizedRoomOwnerException {
+package project.realtimechatapplication.exception.impl;
+
+import org.springframework.http.HttpStatus;
+import project.realtimechatapplication.exception.AbstractException;
+
+public class UnauthorizedRoomOwnerException extends AbstractException {
+
+  @Override
+  public int getStatusCode() {
+    return HttpStatus.UNAUTHORIZED.value();
+  }
+
+  @Override
+  public String getMessage() {
+    return "You are not authorized to delete this chat room.";
+  }
 }
