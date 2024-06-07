4906a5bc5a951b30d65d13e358c2fb362a22e3ff
kyoo0115 <internetkyoomin@gmail.com>
2024-06-07 17:21:33 +0800

    feat: added response dto for delete message

--- src/main/java/project/realtimechatapplication/dto/response/chat/DeleteMessageResponseDto.java
+++ src/main/java/project/realtimechatapplication/dto/response/chat/DeleteMessageResponseDto.java
@@ -1,2 +1,15 @@
-package project.realtimechatapplication.dto.response.chat;public class DeleteMessageResponseDto {
+package project.realtimechatapplication.dto.response.chat;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import project.realtimechatapplication.model.type.MessageStatus;
+
+@Builder
+@Getter
+@Setter
+public class DeleteMessageResponseDto {
+  private Long id;
+  private String roomCode;
+  private MessageStatus status;
 }
