dcea9d184389f866d12ef68124a8d06ce702783a
kyoo0115 <internetkyoomin@gmail.com>
2024-06-07 17:24:25 +0800

    style: fixed the indentation

--- src/main/java/project/realtimechatapplication/handler/OAuth2SuccessHandler.java
+++ src/main/java/project/realtimechatapplication/handler/OAuth2SuccessHandler.java
@@ -1,4 +1,4 @@
-package project.realtimechatapplication.model;
+package project.realtimechatapplication.handler;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
+import project.realtimechatapplication.model.CustomOAuth2User;
 import project.realtimechatapplication.provider.TokenProvider;
 
 @Component
