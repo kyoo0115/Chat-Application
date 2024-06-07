86348e9f1542b59f41264685cd6c1f2b66a92ada
kyoo0115 <internetkyoomin@gmail.com>
2024-06-07 17:33:03 +0800

    feat: added service method for message

--- src/main/java/project/realtimechatapplication/service/impl/MessageServiceImpl.java
+++ src/main/java/project/realtimechatapplication/service/impl/MessageServiceImpl.java
@@ -5,12 +5,20 @@ import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import project.realtimechatapplication.dto.request.chat.ChatDto;
+import project.realtimechatapplication.dto.request.chat.DeleteMessageRequestDto;
+import project.realtimechatapplication.dto.request.chat.EditMessageRequestDto;
+import project.realtimechatapplication.dto.response.chat.DeleteMessageResponseDto;
+import project.realtimechatapplication.dto.response.chat.EditMessageResponseDto;
 import project.realtimechatapplication.dto.response.chat.MessageSendResponseDto;
 import project.realtimechatapplication.entity.ChatRoomEntity;
 import project.realtimechatapplication.entity.MessageEntity;
 import project.realtimechatapplication.entity.UserEntity;
 import project.realtimechatapplication.exception.impl.ChatRoomNotFoundException;
+import project.realtimechatapplication.exception.impl.MessageNotExistException;
+import project.realtimechatapplication.exception.impl.UnauthorizedMessageDeletionException;
+import project.realtimechatapplication.exception.impl.UnauthorizedMessageEditException;
 import project.realtimechatapplication.exception.impl.UserNotFoundException;
+import project.realtimechatapplication.model.type.MessageStatus;
 import project.realtimechatapplication.repository.ChatRoomRepository;
 import project.realtimechatapplication.repository.MessageRepository;
 import project.realtimechatapplication.repository.UserRepository;
@@ -18,6 +26,7 @@ import project.realtimechatapplication.service.MessageService;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class MessageServiceImpl implements MessageService {
 
     private final MessageRepository messageRepository;
@@ -46,8 +55,57 @@ public class MessageServiceImpl implements MessageService {
     }
 
     @Override
-    public ChatDto makeEnterMessageAndSetSessionAttribute(ChatDto chatDto, SimpMessageHeaderAccessor headerAccessor) {
-        headerAccessor.getSessionAttributes().put("username", chatDto.getSender());
-        return chatDto;
+    public ChatDto makeEnterMessageAndSetSessionAttribute(ChatDto dto, SimpMessageHeaderAccessor headerAccessor) {
+        headerAccessor.getSessionAttributes().put("username", dto.getSender());
+        return dto;
+    }
+
+    @Override
+    @Transactional
+    public EditMessageResponseDto editMessage(EditMessageRequestDto dto, String username, Long messageId) {
+
+        UserEntity user = userRepository.findByUsername(username)
+            .orElseThrow(UserNotFoundException::new);
+
+        MessageEntity message = messageRepository.findById(messageId)
+            .orElseThrow(MessageNotExistException::new);
+
+        if (!message.getUser().getId().equals(user.getId())) {
+            throw new UnauthorizedMessageEditException();
+        }
+
+        message.setMessage(dto.getMessage());
+        messageRepository.save(message);
+
+        return EditMessageResponseDto.builder()
+                .messageId(message.getId())
+                .newMessage(message.getMessage())
+                .chatRoomId(message.getChatRoom().getId())
+                .status(MessageStatus.EDIT)
+                .timestamp(message.getModifiedAt())
+                .build();
+    }
+
+    @Override
+    @Transactional
+    public DeleteMessageResponseDto deleteMessage(DeleteMessageRequestDto dto, String username, Long messageId) {
+
+        UserEntity user = userRepository.findByUsername(username)
+                .orElseThrow(UserNotFoundException::new);
+
+        MessageEntity message = messageRepository.findById(messageId)
+                .orElseThrow(MessageNotExistException::new);
+
+        if (!message.getUser().getId().equals(user.getId())) {
+            throw new UnauthorizedMessageDeletionException();
+        }
+
+        messageRepository.delete(message);
+
+        return DeleteMessageResponseDto.builder()
+                .id(message.getId())
+                .roomCode(dto.getRoomCode())
+                .status(MessageStatus.DELETE)
+                .build();
     }
 }
