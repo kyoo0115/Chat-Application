1230ffe7d5aeadef8f405fbb31a0a9e60aecbd09
kyoo0115 <internetkyoomin@gmail.com>
2024-06-07 17:21:58 +0800

    feat: added response dto for edit message

--- src/main/java/project/realtimechatapplication/dto/response/chat/EditMessageResponseDto.java
+++ src/main/java/project/realtimechatapplication/dto/response/chat/EditMessageResponseDto.java
@@ -1,2 +1,17 @@
-package project.realtimechatapplication.dto.response.chat;public class EditMessageResponseDto {
+package project.realtimechatapplication.dto.response.chat;
+
+import java.time.LocalDateTime;
+import lombok.Builder;
+import lombok.Getter;
+import project.realtimechatapplication.model.type.MessageStatus;
+
+@Getter
+@Builder
+public class EditMessageResponseDto {
+  private Long messageId;
+  private String newMessage;
+  private String senderId;
+  private Long chatRoomId;
+  private LocalDateTime timestamp;
+  private MessageStatus status;
 }
