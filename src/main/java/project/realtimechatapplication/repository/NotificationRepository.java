50dd737bc37cf180dafda1ee59aceab106797c47
kyoo0115 <internetkyoomin@gmail.com>
2024-06-10 18:21:14 +0800

    feat: added repository layer for notification

--- src/main/java/project/realtimechatapplication/repository/NotificationRepository.java
+++ src/main/java/project/realtimechatapplication/repository/NotificationRepository.java
@@ -1,2 +1,8 @@
-package project.realtimechatapplication.repository;public interface NotificationRepository {
+package project.realtimechatapplication.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import project.realtimechatapplication.entity.NotificationEntity;
+
+public interface NotificationRepository extends JpaRepository<NotificationEntity, Long> {
+  
 }
