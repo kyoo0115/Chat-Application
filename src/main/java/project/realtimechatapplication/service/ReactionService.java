8aed80e84e681059564d3aed11bb8bdad014dd6c
kyoo0115 <internetkyoomin@gmail.com>
2024-06-10 14:42:25 +0800

    added service for reaction

--- src/main/java/project/realtimechatapplication/service/ReactionService.java
+++ src/main/java/project/realtimechatapplication/service/ReactionService.java
@@ -1,2 +1,17 @@
-package project.realtimechatapplication.service;public interface ReactionService {
+package project.realtimechatapplication.service;
+
+import java.util.List;
+import project.realtimechatapplication.dto.request.reaction.AddReactionRequestDto;
+import project.realtimechatapplication.dto.request.reaction.EditReactionRequestDto;
+import project.realtimechatapplication.dto.request.reaction.ReactionDto;
+
+public interface ReactionService {
+
+  ReactionDto addReaction(AddReactionRequestDto dto, String username);
+
+  ReactionDto editReaction(EditReactionRequestDto dto, String username, Long reactionId);
+
+  void removeReaction(Long reactionId, String username);
+
+  List<ReactionDto> getReactionsByMessageId(Long messageId);
 }
