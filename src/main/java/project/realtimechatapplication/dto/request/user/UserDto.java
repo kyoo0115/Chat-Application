4add9ffe0a2dc6d94296708c2139810b72ad95f7
kyoo0115 <internetkyoomin@gmail.com>
2024-06-10 14:45:59 +0800

    feat: added dto for user info

---
+++ src/main/java/project/realtimechatapplication/dto/request/user/UserDto.java
@@ -0,0 +1,30 @@
+package project.realtimechatapplication.dto.request.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import project.realtimechatapplication.entity.UserEntity;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Builder
+@AllArgsConstructor
+public class UserDto {
+  
+  private Long id;
+  private String username;
+  private String name;
+  private String email;
+
+  public static UserDto from(UserEntity userEntity) {
+    return UserDto.builder()
+        .id(userEntity.getId())
+        .username(userEntity.getUsername())
+        .name(userEntity.getName())
+        .email(userEntity.getEmail())
+        .build();
+  }
+}
