9713391535ca9d9fe5de1109b641c2c3a4e36e7d
kyoo0115 <internetkyoomin@gmail.com>
2024-06-10 18:21:30 +0800

    feat: added dtos for each response and request

---
+++ src/main/java/project/realtimechatapplication/dto/response/notification/MessageNotificationResponseDto.java
@@ -0,0 +1,32 @@
+package project.realtimechatapplication.dto.response.notification;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import project.realtimechatapplication.entity.NotificationEntity;
+import project.realtimechatapplication.model.type.NotificationType;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class MessageNotificationResponseDto {
+  private Long id;
+  private NotificationType type;
+  private Long userId;
+  private Long messageId;
+  private String message;
+
+  public static MessageNotificationResponseDto to(NotificationEntity notificationEntity, String chatRoomName, String senderUsername, String messageBody) {
+    return MessageNotificationResponseDto.builder()
+        .id(notificationEntity.getId())
+        .type(NotificationType.MESSAGE)
+        .userId(notificationEntity.getUser().getId())
+        .messageId(notificationEntity.getMessage() != null ? notificationEntity.getMessage().getId() : null)
+        .message(senderUsername + " has sent a message in " + chatRoomName + ": " + messageBody)
+        .build();
+  }
+}
