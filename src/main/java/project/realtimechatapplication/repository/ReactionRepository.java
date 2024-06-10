ca4e88ba4441695ea43db72d9d0feda6acb143f9
kyoo0115 <internetkyoomin@gmail.com>
2024-06-10 14:42:51 +0800

    feat: created repository layer for reactio

--- src/main/java/project/realtimechatapplication/repository/ReactionRepository.java
+++ src/main/java/project/realtimechatapplication/repository/ReactionRepository.java
@@ -1,2 +1,13 @@
-package project.realtimechatapplication.repository;public interface ReactionRepository {
+package project.realtimechatapplication.repository;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import project.realtimechatapplication.entity.MessageEntity;
+import project.realtimechatapplication.entity.ReactionEntity;
+
+@Repository
+public interface ReactionRepository extends JpaRepository<ReactionEntity, Long> {
+
+  List<ReactionEntity> findByMessage(MessageEntity message);
 }
