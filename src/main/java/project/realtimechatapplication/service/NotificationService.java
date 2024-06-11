b5c1aec13419cc6be63d6bf2e5edd5d937fbb719
kyoo0115 <internetkyoomin@gmail.com>
2024-06-11 23:46:51 +0800

    feat: added notification event listener for mention

--- src/main/java/project/realtimechatapplication/service/NotificationService.java
+++ src/main/java/project/realtimechatapplication/service/NotificationService.java
@@ -5,7 +5,11 @@ import project.realtimechatapplication.dto.response.chat.MessageSendResponseDto;
 import project.realtimechatapplication.model.type.Reaction;
 
 public interface NotificationService {
+
   void createAndSendNotifications(ChatDto chatDto, MessageSendResponseDto responseDto);
+
   void createAndSendReactionNotification(Long messageId, String reactorUsername, Reaction reaction);
-  void createAndSendAddMemberNotification(String chatRoomName, Long addedUser, String addedByUsername);
+
+  void createAndSendAddMemberNotification(String chatRoomName, Long addedUser,
+      String addedByUsername);
 }
