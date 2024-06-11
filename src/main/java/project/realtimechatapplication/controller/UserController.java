3863b5d5eb0f16d97d4a88eecd7fddd91442e768
kyoo0115 <internetkyoomin@gmail.com>
2024-06-11 23:52:14 +0800

    feat: added swagger endpoints

--- src/main/java/project/realtimechatapplication/controller/UserController.java
+++ src/main/java/project/realtimechatapplication/controller/UserController.java
@@ -1,5 +1,8 @@
 package project.realtimechatapplication.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -11,20 +14,23 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import project.realtimechatapplication.dto.request.user.EditUserRequestDto;
 import project.realtimechatapplication.dto.request.user.UserDto;
 import project.realtimechatapplication.dto.response.CustomResponse;
-import project.realtimechatapplication.service.AuthService;
+import project.realtimechatapplication.service.UserService;
 
 @RestController
 @RequestMapping("/api/user")
 @RequiredArgsConstructor
 @Slf4j
+@Tag(name = "사용자 컨트롤러", description = "사용자 관리를 위한 API들")
 public class UserController {
 
-  private final AuthService userService;
+  private final UserService userService;
 
+  @Operation(summary = "사용자 조회", description = "ID로 사용자를 조회합니다.")
   @GetMapping("/{userId}")
   public ResponseEntity<?> viewUser(
       @PathVariable Long userId
@@ -33,6 +39,7 @@ public class UserController {
     return CustomResponse.success(user);
   }
 
+  @Operation(summary = "사용자 수정", description = "ID로 사용자를 수정합니다.")
   @PutMapping("/{userId}")
   public ResponseEntity<?> editUser(
       @PathVariable Long userId,
@@ -43,6 +50,7 @@ public class UserController {
     return CustomResponse.success(updatedUser);
   }
 
+  @Operation(summary = "사용자 삭제", description = "ID로 사용자를 삭제합니다.")
   @DeleteMapping("/{userId}")
   public ResponseEntity<?> deleteUser(
       @PathVariable Long userId,
@@ -50,5 +58,14 @@ public class UserController {
 
     userService.deleteUser(userId, user.getUsername());
     return CustomResponse.success();
+  }
+
+  @GetMapping("/chatroom/{roomCode}")
+  public ResponseEntity<?> searchChatRoomMembers(
+      @PathVariable String roomCode,
+      @RequestParam String query) {
+
+    List<UserDto> members = userService.searchChatRoomMembers(roomCode, query);
+    return CustomResponse.success(members);
   }
 }
