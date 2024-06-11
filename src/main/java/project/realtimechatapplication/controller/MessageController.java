a670196ec5c28e913874cfefe93c1bae14ea0716
kyoo0115 <internetkyoomin@gmail.com>
2024-06-11 23:45:45 +0800

    feat: added swagger endpoints

--- src/main/java/project/realtimechatapplication/controller/MessageController.java
+++ src/main/java/project/realtimechatapplication/controller/MessageController.java
@@ -1,15 +1,17 @@
 package project.realtimechatapplication.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.catalina.User;
 import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -30,6 +32,7 @@ import project.realtimechatapplication.service.NotificationService;
 @RequiredArgsConstructor
 @CrossOrigin
 @Slf4j
+@Tag(name = "메세지 컨트롤러", description = "메세지 관리를 위한 API들")
 public class MessageController {
 
   private final MessageService messageService;
@@ -41,7 +44,8 @@ public class MessageController {
       @Payload @Valid ChatDto chatDto,
       SimpMessageHeaderAccessor headerAccessor) {
     log.info("enterChatRoom : {}", chatDto.getSender());
-    ChatDto newChatDto = messageService.makeEnterMessageAndSetSessionAttribute(chatDto, headerAccessor);
+    ChatDto newChatDto = messageService.makeEnterMessageAndSetSessionAttribute(chatDto,
+        headerAccessor);
     msgOperation.convertAndSend("/topic/chat/room/" + chatDto.getRoomCode(), newChatDto);
   }
 
@@ -50,30 +54,45 @@ public class MessageController {
       @Payload @Valid ChatDto chatDto) {
     log.info("sendChatMessage : {}", chatDto.getSender());
     MessageSendResponseDto messageSendResponseDto = messageService.sendMessage(chatDto);
-    msgOperation.convertAndSend("/topic/chat/room/" + chatDto.getRoomCode(), messageSendResponseDto);
+    msgOperation.convertAndSend("/topic/chat/room/" + chatDto.getRoomCode(),
+        messageSendResponseDto);
     notificationService.createAndSendNotifications(chatDto, messageSendResponseDto);
   }
 
+  @Operation
+      (
+          summary = "해당 메세지 수정",
+          description = "사용자는 본인 메세지만 수정할 수 있습니다.<br>"
+              + "[messageId]는 사용자는 본인 메세지의 [id]를 지정할 수 있습니다."
+      )
   @PutMapping("/chat/{messageId}")
   public ResponseEntity<?> editChatMessage(
       @RequestBody @Valid EditMessageRequestDto dto,
       @AuthenticationPrincipal final User user,
-               @PathVariable Long messageId
+      @PathVariable Long messageId
   ) {
-    log.info("editChatMessage : {}", dto.getSender());
-    EditMessageResponseDto messageEditResponseDto = messageService.editMessage(dto, user.getUsername(), messageId);
+    log.info("editChatMessage : {}", user.getUsername());
+    EditMessageResponseDto messageEditResponseDto = messageService.editMessage(dto,
+        user.getUsername(), messageId);
     msgOperation.convertAndSend("/topic/chat/room/" + dto.getRoomCode(), messageEditResponseDto);
     return CustomResponse.success(messageEditResponseDto);
   }
 
+  @Operation
+      (
+          summary = "해당 메세지 삭제",
+          description = "사용자는 본인 메세지만 삭제할 수 있습니다.<br>"
+              + "[messageId]는 사용자는 본인 메세지의 [id]를 지정할 수 있습니다."
+      )
   @DeleteMapping("/chat/{messageId}")
   public ResponseEntity<?> deleteChatMessage(
       @RequestBody @Valid DeleteMessageRequestDto dto,
       @AuthenticationPrincipal final User user,
-               @PathVariable Long messageId
+      @PathVariable Long messageId
   ) {
-    log.info("deleteChatMessage : {}", dto.getSender());
-    DeleteMessageResponseDto deleteMessageResponseDto = messageService.deleteMessage(dto, user.getUsername(), messageId);
+    log.info("deleteChatMessage : {}", user.getUsername());
+    DeleteMessageResponseDto deleteMessageResponseDto = messageService.deleteMessage(dto,
+        user.getUsername(), messageId);
     msgOperation.convertAndSend("/topic/chat/room/" + dto.getRoomCode(), deleteMessageResponseDto);
     return CustomResponse.success(deleteMessageResponseDto);
   }
