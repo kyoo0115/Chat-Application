136aee26eef4222758b1a3c17532574fdc420635
kyoo0115 <internetkyoomin@gmail.com>
2024-06-11 23:43:49 +0800

    feat: added config for swagger

--- src/main/java/project/realtimechatapplication/config/SwaggerConfig.java
+++ src/main/java/project/realtimechatapplication/config/SwaggerConfig.java
@@ -1,2 +1,25 @@
-package project.realtimechatapplication.config;public class SwaggerConfig {
+package project.realtimechatapplication.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+  @Bean
+  public OpenAPI openAPI() {
+    return new OpenAPI()
+        .components(new Components())
+        .info(apiInfo());
+  }
+
+  private Info apiInfo() {
+    return new Info()
+        .title("Realtime Chat Application API")
+        .description("API for Realtime Chat Application")
+        .version("1.0.0");
+  }
 }
